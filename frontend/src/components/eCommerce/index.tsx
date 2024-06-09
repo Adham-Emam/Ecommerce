002b9c580e0bce34ba2897369b8bbab7ae3f9b5f
Adham_Emam <mrado372002@gmail.com>
2024-06-09 18:43:48 +0300

    heroku setup

--- frontend/src/components/eCommerce/index.tsx
+++
@@ -1,5 +0,0 @@
-import HeaderBasket from "./HeaderBasket/HeaderBasket";
-import Category from "./Category/Category";
-import Product from "./Product/Product";
-
-export { HeaderBasket, Product, Category };
--- frontend/src/pages/index.ts
+++
@@ -1,9 +0,0 @@
-import Home from "./Home";
-import Categories from "./Categories";
-import Products from "./Products";
-import About from "./About";
-import Login from "./Login";
-import Register from "./Register";
-import Error from "./Error";
-
-export { Home, Categories, Products, About, Login, Register, Error };
